fbd1777f3ae20779e5cc6b487bc7e60701bfe862
piotr wasilewski <piotr.wasilewski4202gmail.com>
2022-05-16 10:17:30 +0200

    initial commit

---
+++ register-service/src/test/java/com/sportsapp/registerservice/RegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportsapp.registerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
